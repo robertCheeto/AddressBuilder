6577dc9031697f4751dfd8bbbc7515d827cb63dc
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 13:29:03 -0400

    * added shippingInformation variables for user inputs * created second StringBuilder for shipping information * added both StringBuilder variables to String customerInformation so information can be printed out in one variable

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -6,37 +6,49 @@ public class AddressBuilder {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         StringBuilder billingInformation = new StringBuilder();
-
-        System.out.println("Enter the following information:\n");
+        StringBuilder shippingInformation = new StringBuilder();
 
         System.out.print("Enter your name: ");
         String userName = keyboard.nextLine();
-        billingInformation.append(userName);
+        billingInformation.append(userName + "\n\n");
 
+        System.out.println("\n*** Enter Billing Information ***");
         System.out.print("Billing Street: ");
         String billingStreet = keyboard.nextLine();
-        billingInformation.append(billingStreet);
+        billingInformation.append("Billing Address:\n" + billingStreet + "\n");
 
         System.out.print("Billing City: ");
         String billingCity = keyboard.nextLine();
-        billingInformation.append(billingCity);
+        billingInformation.append(billingCity + ", ");
 
         System.out.print("Billing State: ");
         String billingState = keyboard.nextLine();
-        billingInformation.append(billingState);
+        billingInformation.append(billingState + " ");
 
         System.out.print("Billing Zipcode: ");
         int billingZip = keyboard.nextInt();
         billingInformation.append(billingZip);
+        keyboard.nextLine();
 
+        System.out.println("\n*** Enter Shipping Information ***");
+        System.out.print("Shipping Street: ");
+        String shippingStreet = keyboard.nextLine();
+        shippingInformation.append("\nShipping Address:\n" + shippingStreet + "\n");
 
-        System.out.print("\nShipping Street: ");
         System.out.print("Shipping City: ");
-        System.out.print("Shipping State: ");
-        System.out.print("Shipping Zipcode: ");
+        String shippingCity = keyboard.nextLine();
+        shippingInformation.append(shippingCity + ", ");
 
-        String customerInformation = billingInformation.toString();
-        System.out.printf("\n%s",customerInformation);
+        System.out.print("Shipping State: ");
+        String shippingState = keyboard.nextLine();
+        shippingInformation.append(shippingState + " ");
+
+        System.out.print("Shipping Zipcode: ");
+        String shippingZipcode = keyboard.nextLine();
+        shippingInformation.append(shippingZipcode);
+
+        String customerInformation = billingInformation.toString() + "\n" + shippingInformation.toString();
+        System.out.printf("\n*** Order Information ***\n%s\n************************",customerInformation);
 
 
 
